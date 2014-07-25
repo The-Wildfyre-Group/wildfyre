718235fd11df0c9e206120525e3e8780c9440c9c
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-07-25 03:02:25 -0400

    add current user, new user, and http methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def http_basic_authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "spa" && password == "spa"
+      end
+    else
+      true
+    end
+  end
+  
+  # users
+  
+  def new_user
+    @user ||= User.new
+  end
+  
+  def current_user
+    @current_user ||= User.find_by_authentication_token(cookies[:authentication_token]) if cookies[:authentication_token]
+  end
+  
+  helper_method :new_user, :current_user
+  
+  
 end
