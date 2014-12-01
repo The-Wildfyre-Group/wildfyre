ac3e5c82d5f2d5ca2ecc37e5eeb7fd03fefa252a
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-12-01 16:02:15 -0500

    Add friendly id and full_name method to contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,17 @@
 class Contact < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :use_for_slug, use: :slugged
+  
+  def use_for_slug
+    existing_contact = Contact.where('first_name = ?', self.first_name).where('last_name = ?', self.last_name)
+    if existing_contact.present?
+      "#{first_name} #{last_name} #{existing_contact.count}"
+    else
+      "#{first_name} #{last_name}"
+    end
+  end
+  
+  def full_name
+    first_name && last_name && "#{first_name} #{last_name}" || email
+  end
 end
